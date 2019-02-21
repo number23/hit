eea72ffbbfbb25c3f3f4f7febc55b9af66f7432c
number23 <number23.cn@gmail.com>
2019-02-21 22:04:14 +0800

    fix download.go

--- download.go
+++ download.go
@@ -23,14 +23,6 @@ func main() {
 // DownloadFile will download a url to a local file. It's efficient because it will
 // write as it downloads and not load the whole file into memory.
 func DownloadFile(filepath string, url string) error {
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
 	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
@@ -43,9 +35,19 @@ func DownloadFile(filepath string, url string) error {
 		return fmt.Errorf("bad status: %s", resp.Status)
 	}
 
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
+		return err
+	}
+
+	if err := out.Close(); err != nil {
 		return err
 	}
 
@@ -54,15 +56,15 @@ func DownloadFile(filepath string, url string) error {
 
 func DownloadFileFromFtp(c *ftp.ServerConn, fname string) error {
 	r, err := c.Retr(fname)
-	defer r.Close()
-
 	if err != nil {
 		return err
 	}
+	defer r.Close()
 
 	buf, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
+
 	return ioutil.WriteFile(fname, buf, 0644)
 }
